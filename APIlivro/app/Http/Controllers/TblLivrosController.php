e364d92a81dfcd7dd6b3c537daa92595e5ba31ec
pepeu007 <24pedrinho@gmail.com>
2024-08-02 22:53:12 -0300

    novas funções

---
+++ APIlivro/app/Http/Controllers/TblLivrosController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TblLivrosController extends Controller
+{
+    // construir o crud.
+
+
+    //Mostrar todos os registros da tabela livros
+    //Crud -> Read(leitura) Select/Visualizar
+    public function index(){
+
+    }
+    //Mostar um tipo de leitura especifico
+    //Crud -> Read(leitura) Select/Visualizar
+    public function show(){
+
+    }
+
+    //Cadastrar
+    //Crud -> Create(criar/cadastrar)
+    public function store(){
+
+    }
+    //Alterar egistros
+    //Crud -> update(alterar)
+    public function update(){
+
+
+    //Deletar os registros
+    //Crud -> delete(apagar)
+    }
+    public function destroy(){
+
+    }
+}
